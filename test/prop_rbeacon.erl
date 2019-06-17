06844b73558c77e8d04a61edf5509d66ae994e83
JOrge Becerra Rios <j.brios@udc.es>
2019-06-17 11:52:49 +0200

    añadidas funcionalidades julio

--- test/prop_rbeacon.erl
+++ test/prop_rbeacon.erl
@@ -10,14 +10,16 @@ prop_can_send_and_receive_any_string() ->
                 ok = rbeacon:publish(Service, unicode:characters_to_binary(String)),
                 
                 {ok, Client} = rbeacon:new(9999),
-                ok = rbeacon:subscribe(Client, ""),
+                ok = rbeacon:subscribe(Client, ""),%%se subscribe a oir mj y no aplica ningun filtro ""
                 
                 {ok, Msg, _Addr} = rbeacon:recv(Client),
                 
                 ok = rbeacon:close(Service),
                 ok = rbeacon:close(Client),
-                
+                %collect para capturar estadisticas de las pruebas
                 % collect(length(String),
+                %measure para capturar tb estadisticas -> le da una etiqueta a la medida
+                %measure saca min max etc etc 
                 measure("Lonxitude do string",
                         length(String),
                 % the assert must be the last thing the property does
--- test/prop_rbeacon_stateful.erl
+++ test/prop_rbeacon_stateful.erl
@@ -3,31 +3,36 @@
 
 -export([command/1, initial_state/0, next_state/3,
          precondition/2, postcondition/3]).
--export([close/1, publish/2]).
+-export([close/1, publish/2, broadcast_ip/1, subscribe/2, recv/1, unsubscribe/1]).
 
 -record(test_state,{rbeacon=null,
-                    message=null}).
+                    message=null,
+                    subscribed=false}).
 
 %% @doc Default property
 prop_prop_rbeacon_stateful() ->
     ?FORALL(Cmds, commands(?MODULE),
             begin
-                {H, S, Res} = run_commands(?MODULE, Cmds),
+                {H, S, Res} = run_commands(?MODULE, Cmds),% devuelve  historia comandos,estado final del test acumulado, resultado final 
                 ?WHENFAIL(io:format("History: ~p\nState: ~p\nResult: ~p\n",
                                     [H, S, Res]),
                           aggregate(command_names(Cmds), Res =:= ok))
             end).
 
 %% @doc Returns the state in which each test case starts.
+%
 initial_state() ->
-    #test_state{}.
+    #test_state{}.%registro 
 
 %% @doc Command generator, S is the current state
 command(S) ->
     oneof([{call, rbeacon, new, [user_udp_port()]},
            {call, ?MODULE, close, [S#test_state.rbeacon]},
            {call, ?MODULE, publish, [S#test_state.rbeacon, binary()]},
-           {call, rbeacon, hostname, [S#test_state.rbeacon]}
+           {call, ?MODULE, broadcast_ip, [S#test_state.rbeacon]},
+           {call, ?MODULE, subscribe, [S#test_state.rbeacon,""]},%filtra todos los mj ""
+           {call, ?MODULE, recv, [S#test_state.rbeacon]},
+           {call, ?MODULE, unsubscribe, [S#test_state.rbeacon]}%filtra todos los mj ""
           ]).
 
 % UDP ports 49152 through 65535
@@ -38,13 +43,19 @@ user_udp_port() ->
 next_state(S, V, {call, rbeacon, new, _Port}) ->
     S#test_state{rbeacon = V};
 next_state(S, _V, {call, ?MODULE, close, _Beacon}) ->
-    S#test_state{rbeacon = null};
+    S#test_state{rbeacon = null,subscribed = false,message = null};
 next_state(S, _V, {call, ?MODULE, publish, [_Beacon, Binary]}) ->
-    S#test_state{message = Binary}.
-%next_state(S, _V, {call, rbeacon, hostname, _Beacon}) ->
- %   S#test_state{rbeacon=rbeacon}.
+    S#test_state{message = Binary};
+next_state(S, _V, {call, ?MODULE, broadcast_ip, _Beacon}) ->
+    S=S;
+next_state(S, _V, {call, ?MODULE, subscribe,[_Beacon, _Binary]}) ->
+     S#test_state{subscribed = true};
+next_state(S, _V, {call, ?MODULE, recv, _Beacon}) ->
+    S=S;
+next_state(S, _V, {call, ?MODULE, unsubscribe,[_Beacon]}) ->
+     S#test_state{subscribed = false}.
 
-
+% para indicar cuando NO quiero un comando con esto indico que las llamadas pertinentes son new close new close new close
 %% @doc Precondition, checked before command is added to the command sequence.
 precondition(S, {call, rbeacon, new, _Port}) ->
     S#test_state.rbeacon == null;
@@ -52,8 +63,14 @@ precondition(S, {call, ?MODULE, close, _Beacon}) ->
     S#test_state.rbeacon =/= null;
 precondition(S, {call, ?MODULE, publish, [_Beacon, _Binary]}) ->
     S#test_state.rbeacon =/= null;
-%precondition(S, {call, rbeacon, hostname, _Beacon}) ->
-%    S#test_state.rbeacon =/= null;
+precondition(S, {call, ?MODULE, broadcast_ip, _Beacon}) ->
+    S#test_state.rbeacon =/= null;
+precondition(S, {call, ?MODULE, subscribe, [_Beacon, _Binary]}) ->
+    S#test_state.rbeacon =/= null;
+precondition(S, {call, ?MODULE, recv, [_Beacon]}) ->
+    (S#test_state.rbeacon =/= null ) and (S#test_state.message =/= null) and (S#test_state.subscribed == true);
+precondition(S, {call, ?MODULE, unsubscribe, [_Beacon]}) ->
+    (S#test_state.rbeacon =/= null ) and (S#test_state.subscribed == true);
 precondition(_S, _Call) ->
     false.
 
@@ -64,8 +81,25 @@ postcondition(_S, {call, rbeacon, new, _Port}, {ok, Beacon}) ->
     is_pid(Beacon);
 postcondition(_S, {call, ?MODULE, publish, [_Beacon, _Binary]}, ok) ->
     true;
+    
+postcondition(_S, {call, ?MODULE, broadcast_ip, _Beacon}, {msg}) ->%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%555????????????????????''''
+	false;
+	%true;
+    %is_bitstring (msg);
+    %is_bitstring (unicode:characters_to_binary(msg));
+    %is_binary(msg);
+postcondition(_S, {call, ?MODULE, subscribe, [_Beacon, _Binary]}, ok) ->
+    true;
+postcondition(_S, {call, ?MODULE, recv, _Beacon}, {ok, msg, dir}) ->%deberia recibir algo como {ok,<<"String">>,{192,168,1,35}}
+    %msg==_S#test_state.message;
+    false;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%555????????????????????''''
+postcondition(_S, {call, ?MODULE, unsubscribe, [_Beacon]}, ok) ->
+    true;
 postcondition(_S, _Call, _Res) ->
     true.
+
+
+     
 
 %% Wrappers para el modelo
 close({ok, Beacon}) ->
@@ -73,3 +107,15 @@ close({ok, Beacon}) ->
 
 publish({ok, Beacon}, Binary) ->
     rbeacon:publish(Beacon, Binary).
+    
+broadcast_ip({ok, Beacon}) ->
+    rbeacon:broadcast_ip(Beacon).
+
+subscribe({ok, Beacon}, Binary) ->
+    rbeacon:subscribe(Beacon, Binary).
+
+recv({ok, Beacon}) ->
+    rbeacon:recv(Beacon).
+
+unsubscribe({ok, Beacon}) ->
+    rbeacon:unsubscribe(Beacon).
