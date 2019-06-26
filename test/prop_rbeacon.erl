4f5752630e395bd081ad1e651729e16b8923ce50
JOrge Becerra Rios <j.brios@udc.es>
2019-06-26 13:32:14 +0200

    tratando de solucionar el equals de message en rama master

--- test/prop_rbeacon.erl
+++ test/prop_rbeacon.erl
@@ -1,6 +1,8 @@
 -module(prop_rbeacon).
 -include_lib("proper/include/proper.hrl").
 
+%-export([random_port/0]).
+
 prop_can_send_and_receive_any_string() ->
     numtests(10,
        ?FORALL(String, string(),
@@ -29,5 +31,44 @@ prop_can_send_and_receive_any_string() ->
             end
              )
         ).
+        
+%49152-65535: Son puertos dinámicos o puertos privados
+random_port() ->
+    49155. 
+    
+%Prueba basada en propiedades: Probamos que se puede crear el beacon y que este es capaz de compiortarse como tal
+%Probamos por ejemplo que el beacon es capaz de publicar ,de subscribirse ,de parar de comunicar y  de recibir usando el puerto escogido aleatoriamente
+prop_can_use_different_port() ->
+    numtests(10,
+       ?FORALL(String,  string(),
+            begin
+				Port=random_port(),
+                {ok, Service} = rbeacon:new(Port),
+                ok = rbeacon:set_interval(Service, 100),
+                ok = rbeacon:publish(Service, unicode:characters_to_binary("Hola Ana")),
+                
+                {ok, Client_and_server} = rbeacon:new(Port),
+                ok = rbeacon:subscribe(Client_and_server, ""),
+                
+                {ok, _, _Addr} = rbeacon:recv(Client_and_server),
+                
+                ok = rbeacon:set_interval(Client_and_server, 100),
+                ok=rbeacon:silence(Service),%silenciamos el beacon Service para que este no escuche su propio mj y fracase por tanto la prueba
+                ok = rbeacon:publish(Client_and_server, unicode:characters_to_binary("Hola Sara")),
+                
+                ok = rbeacon:subscribe(Service, ""),
+                {ok, Msg_2, _Addr} = rbeacon:recv(Client_and_server),
+                
+                ok = rbeacon:close(Service),
+                ok = rbeacon:close(Client_and_server),
+                measure(
+						"Lonxitude do string",
+                        length("Hola Ana"),
+                
+               equals(unicode:characters_to_binary("Hola Sara"), Msg_2)
+                        )
 
-
+            end
+             )
+        ).
+        
