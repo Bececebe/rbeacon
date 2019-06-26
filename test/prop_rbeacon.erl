52f33ee032c937514bf0b2beb0014dd24d1a9ba2
JOrge Becerra Rios <j.brios@udc.es>
2019-06-26 13:29:20 +0200

    commit borrado metodo loquesea

--- test/prop_rbeacon.erl
+++ test/prop_rbeacon.erl
@@ -72,31 +72,3 @@ prop_can_use_different_port() ->
              )
         ).
         
-prop_loquesea() ->
-    numtests(10,
-       ?FORALL(String, string(),
-            begin
-                {ok, Service} = rbeacon:new(9999),
-                ok = rbeacon:set_interval(Service, 100),
-                ok = rbeacon:publish(Service, unicode:characters_to_binary(String)),
-                
-                {ok, Client} = rbeacon:new(9999),
-                ok = rbeacon:subscribe(Client, ""),%%se subscribe a oir mj y no aplica ningun filtro ""
-                
-                {ok, Msg, _Addr} = rbeacon:recv(Client),
-                
-                ok = rbeacon:close(Service),
-                ok = rbeacon:close(Client),
-                %collect para capturar estadisticas de las pruebas
-                % collect(length(String),
-                %measure para capturar tb estadisticas -> le da una etiqueta a la medida
-                %measure saca min max etc etc 
-                measure("Lonxitude do string",
-                        length(String),
-                % the assert must be the last thing the property does
-                equals(unicode:characters_to_binary(String), Msg)
-                        )
-
-            end
-             )
-        ).
